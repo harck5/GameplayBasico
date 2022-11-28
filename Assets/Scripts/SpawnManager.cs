8881bc340a278136ab3ed3c1e83f27b202e9c61b
harck5 <raaaaullopez@gmail.com>
2022-11-28 08:14:06 +0100

    wa

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+    
+    private float spawnRangeX = 14f;
+    private float spawnPosZ = 19f;
+
+    private float startDelay = 1.5f;
+    private float spawnInterval = 2f;
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+    /*private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 10), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }*/
+    private void SpawnRandomAnimal()
+        {
+        animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], position: RandomSpawnPos(), animalPrefabs[animalIndex].transform.rotation);
+        }
+    private Vector3 RandomSpawnPos()
+    {
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        return new Vector3(randomX, 0, spawnPosZ);
+    }
+}
